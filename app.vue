bd0a9469d40b0a283ffe24dd8590fe787ba968fe
mhamad raad <hamaraad883@gmail.com>
2025-06-05 21:43:13 +0300

    add structure to the app

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="h-[100vh] flex justify-center items-center">
-    <h1 class="text-5xl font-bold text-blue-600">Welcome</h1>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
 
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Navbar />
+
+    <main class="p-6">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import Navbar from '~/components/Layout/Navbar.vue';
+</script>
