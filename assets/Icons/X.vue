77edcb257ce68f6d9e85cbd71a6773da3ab34382
mhamad raad <hamaraad883@gmail.com>
2025-06-09 02:15:48 +0300

    add the needed assets for modal

---
+++ assets/Icons/X.vue
@@ -0,0 +1,81 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 154 148"
+    fill="none"
+    :class="iconClass"
+  >
+    <g filter="url(#filter0_d_6_5705)">
+      <rect x="21" y="16" width="31" height="30" rx="15" fill="white" />
+      <rect
+        x="21.5"
+        y="16.5"
+        width="30"
+        height="29"
+        rx="14.5"
+        stroke="black"
+        :stroke-opacity="0.93"
+      />
+      <line
+        x1="30.3391"
+        y1="25.6326"
+        x2="43.3391"
+        y2="37.6326"
+        stroke="black"
+      />
+      <line x1="42.3817" y1="25.323" x2="31.3817" y2="38.323" stroke="black" />
+    </g>
+    <defs>
+      <filter
+        id="filter0_d_6_5705"
+        x="-78.024"
+        y="-83.024"
+        width="231.048"
+        height="230.048"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feMorphology
+          radius="0.024"
+          operator="dilate"
+          in="SourceAlpha"
+          result="effect1_dropShadow_6_5705"
+        />
+        <feOffset dx="1" dy="1" />
+        <feGaussianBlur stdDeviation="50" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="BackgroundImageFix"
+          result="effect1_dropShadow_6_5705"
+        />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect1_dropShadow_6_5705"
+          result="shape"
+        />
+      </filter>
+    </defs>
+  </svg>
+</template>
+
+<script setup>
+defineProps({
+  iconClass: {
+    type: String,
+    default: 'w-[154px] h-[148px]',
+  },
+});
+</script>
