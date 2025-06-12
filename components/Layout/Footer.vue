d33c3c0290b1f2e85093b31f58a64f33cb540696
mhamad raad <hamaraad883@gmail.com>
2025-06-12 03:36:26 +0300

    add footer to the app

--- components/Layout/Footer.vue
+++ components/Layout/Footer.vue
@@ -1,2 +1,5 @@
-<template></template>
->
+<template>
+  <footer class="w-full text-center py-4 border-t text-sm text-gray-500">
+    &copy; {{ new Date().getFullYear() }} Miswag. All rights reserved.
+  </footer>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -5,9 +5,11 @@
     <main class="px-3 lg:px-6 pt-36">
       <slot></slot>
     </main>
+    <Footer />
   </div>
 </template>
 
 <script setup>
 import Navbar from '~/components/Layout/Navbar/index.vue';
+import Footer from '~/components/Layout/Footer.vue';
 </script>
