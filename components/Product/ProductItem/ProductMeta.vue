95872db3d6458d443f4543af569f6fd3608953fb
mhamad raad <hamaraad883@gmail.com>
2025-06-12 03:52:19 +0300

    fix minor design flaw in product item

--- components/Product/ProductItem/ProductMeta.vue
+++ components/Product/ProductItem/ProductMeta.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="flex flex-col gap-2 text-sm text-[#575757] relative">
+  <div class="flex flex-col gap-2 text-sm text-[#575757] relative h-full">
     <h3 class="text-right line-clamp-2" dir="rtl">
       <span class="font-bold">{{ product?.brand }}</span>
       {{ product?.title }}
     </h3>
 
-    <div class="flex flex-col justify-between gap-1 text-right">
+    <div class="flex flex-col justify-between gap-1 text-right pb-8">
       <h3 class="text-sm text-[#141414] font-bold">
         {{ `${price} ${product?.price?.currency}` }}
       </h3>
@@ -23,28 +23,30 @@
         <p class="text-xs text-[#575757] font-semibold">هدية</p>
         <GiftIcon />
       </div>
-      <div class="w-full flex justify-end">
-        <div
-          class="px-1 py-[3.5px] flex items-center gap-1 rounded-full w-fit mt-[2px]"
-          :style="{
-            backgroundColor: product?.merchant?.bgColor || '#00000',
-          }"
-        >
-          <h6 class="text-xs text-accent font-medium">
-            {{ product?.merchant?.title }}
-          </h6>
+    </div>
 
-          <BagIcon />
-          <!-- Uncomment the following line if you want to show the merchant icon, it is commented because the link provided does not work -->
-          <!-- <img
-            v-show="product?.merchant?.icon"
-            :src="product?.merchant?.icon"
-            alt="Merchant Icon"
-            class="w-4 h-4 rounded-full"
-          /> -->
-        </div>
+    <div class="absolute bottom-0 w-full flex justify-end">
+      <div
+        class="px-1 py-[3.5px] flex items-center gap-1 rounded-full w-fit mt-[2px]"
+        :style="{
+          backgroundColor: product?.merchant?.bgColor || '#00000',
+        }"
+      >
+        <h6 class="text-xs text-accent font-medium">
+          {{ product?.merchant?.title }}
+        </h6>
+
+        <BagIcon />
+        <!-- Uncomment the following line if you want to show the merchant icon, it is commented because the link provided does not work -->
+        <!-- <img
+          v-show="product?.merchant?.icon"
+          :src="product?.merchant?.icon"
+          alt="Merchant Icon"
+          class="w-4 h-4 rounded-full"
+        /> -->
       </div>
     </div>
+
     <ColorDots
       v-if="properties?.shouldShowVariations && product?.colors?.length"
       :colors="product.colors"
@@ -52,6 +54,7 @@
     />
   </div>
 </template>
+
 <script setup>
 import ColorDots from './ColorDots.vue';
 
