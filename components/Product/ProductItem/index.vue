acecb94bfe3106ac1a40e03df44bf7cc6778d56e
mhamad raad <hamaraad883@gmail.com>
2025-06-28 22:39:20 +0300

    fix minor bug in details logic

--- components/Product/ProductItem/index.vue
+++ components/Product/ProductItem/index.vue
@@ -1,7 +1,8 @@
 <template>
   <button
     type="button"
-    @click="$emit('click')"
+    @click="$emit('click', product)"
+    :disabled="!product.isAvailable"
     class="w-[167.5px] h-[316px] p-2 flex flex-col gap-3 bg-accent border border-skeleton rounded-lg"
   >
     <ProductImage :product="product" :properties="properties" />
@@ -23,6 +24,8 @@ const props = defineProps({
     default: () => ({}),
   },
 });
+
+console.log(props);
 
 const { product, properties } = props;
 </script>
